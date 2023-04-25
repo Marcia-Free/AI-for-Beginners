68995388956905073c08f713e2e47dac42067fcd
Marcia Freeman <MarciaFreeman2011@gmail.com>
2023-04-25 16:09:53 -0500

    Autopilot Tank
    Completed Dot and Cross product and built an autopilot system for the tank to steer itself to the goal.

--- Assets/1-Mathematics of AI/Calculate Distance/DriveTank.cs
+++ Assets/1-Mathematics of AI/Calculate Distance/DriveTank.cs
@@ -8,14 +8,57 @@ public class DriveTank : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
+    float autoSpeed = 2f;
+    float autoRotationSpeed = 0.05f;
     public GameObject fuel;
+    bool autopilot = false;
 
-    void Start()
+    void AutoPilot()
     {
-
+        CalculateAngle();
+        transform.position += transform.up * autoSpeed * Time.deltaTime;
     }
 
-    void CalculateDistance()
+    void CalculateAngle()
+    {
+        //Dot product
+        Vector3 tankForward = transform.up;
+        Vector3 fuelDirection = fuel.transform.position - transform.position;
+
+        Debug.DrawRay(transform.position, tankForward * 10, Color.green, 5);
+        Debug.DrawRay(transform.position, fuelDirection, Color.red, 5);
+
+        float dot = (tankForward.x * fuelDirection.x) + (tankForward.y * fuelDirection.y);
+        float angle = Mathf.Acos(dot / (tankForward.magnitude * fuelDirection.magnitude));
+
+        //Debug.Log("Angle: " + angle * Mathf.Rad2Deg);
+        //Unity built in way of handling angles
+        //Debug.Log("Unity Angle: " + Vector3.Angle(tankForward, fuelDirection));
+
+        int clockwise = 1;
+        if(Cross(tankForward, fuelDirection).z < 0)
+        {
+            clockwise = -1;
+        }
+
+        if((angle * Mathf.Rad2Deg) > 10)
+        {
+            transform.Rotate(0, 0, angle * Mathf.Rad2Deg * clockwise * autoRotationSpeed);
+        }
+    }
+
+    Vector3 Cross(Vector3 v, Vector3 w)
+    {
+        //Cross product calculation
+        //Unity has a method that handled this but this is how you would calculate if that was not available
+        float xMult = (v.y * w.z) - (v.z * w.y);
+        float yMult = (v.x * w.z) - (v.z * w.x);
+        float zMult = (v.x * w.y) - (v.y * w.x);
+
+        return (new Vector3(xMult, yMult, zMult));
+    }
+
+    float CalculateDistance()
     {
         //Pythagorus theorem to calculate distance
         float x = Mathf.Pow((transform.position.x - fuel.transform.position.x),2);
@@ -29,15 +72,12 @@ public class DriveTank : MonoBehaviour
 
         Vector3 tankToFuel = tankPos - fuelPos;
 
-        Debug.Log("Distance: " + distance);
-        Debug.Log("Unity Distance: " + uDistance);
-        Debug.Log("V Magnitude : " + tankToFuel.magnitude);
+        //Debug.Log("Distance: " + distance);
+        //Debug.Log("Unity Distance: " + uDistance);
+        //Debug.Log("V Magnitude : " + tankToFuel.magnitude);
         //Faster operation then magnitude
-        Debug.Log("V SqMagnitude : " + tankToFuel.sqrMagnitude);
-
-
-
-
+        //Debug.Log("V SqMagnitude : " + tankToFuel.sqrMagnitude);
+        return distance;
     }
 
     void LateUpdate()
@@ -61,6 +101,22 @@ public class DriveTank : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             CalculateDistance();
+            CalculateAngle();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            autopilot = !autopilot;
+        }
+
+        if(CalculateDistance() < 3)
+        {
+            autopilot = false;
+        }
+
+        if (autopilot)
+        {
+            AutoPilot();
         }
 
     }
