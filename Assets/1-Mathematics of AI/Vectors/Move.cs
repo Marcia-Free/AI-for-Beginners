e195481bb36ccbb2be82e6440ebc415bbff44001
Marcia Freeman <MarciaFreeman2011@gmail.com>
2023-04-24 21:40:25 -0500

    Vectors
    Lesson Complete

---
+++ Assets/1-Mathematics of AI/Vectors/Move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 2f;
+
+    void Start() 
+    {
+        //will only add on the vector amount of the goal, it wont take into account the pig start position
+        //this.transform.Translate(goal.transform.position);
+
+        //direction = goal.transform.position - this.transform.position;
+
+
+        //this.transform.position = this.transform.position + direction;
+        //same as line above just a different way to write this line
+        //this.transform.Translate(direction);
+
+
+    }
+
+    private void LateUpdate() 
+    {
+        direction = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if (direction.magnitude > 2)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+    }
+}
