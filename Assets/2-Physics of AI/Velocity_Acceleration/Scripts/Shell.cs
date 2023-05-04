19a6243c5541cc8c49f7425a15ad4d9db9f617f0
Marcia Freeman <MarciaFreeman2011@gmail.com>
2023-05-03 19:24:05 -0500

    Acceleration Lesson complete

---
+++ Assets/2-Physics of AI/Velocity_Acceleration/Scripts/Shell.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public GameObject explosion;
+    float speed = 0f;
+    float yspeed = 0f;
+    float mass = 10f;
+    float force = 1f;
+    float drag = 1f;
+    //Gravity on earth: meters per second squared
+    float gravity = -9.8f;
+    float gAccel;
+    float acceleration;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank" || col.gameObject.tag == "ground")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Units of Force https://www.npl.co.uk/resources/q-a/what-is-the-si-unit-of-force
+        acceleration = force / mass;
+        speed += acceleration * 1;
+        gAccel = gravity / mass;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        speed *= (1 - Time.deltaTime * drag);
+        yspeed += gAccel * Time.deltaTime;
+        transform.Translate(0, yspeed, speed);
+
+
+    }
+}
